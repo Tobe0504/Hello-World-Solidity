6f31f2a365f97a396b589ce65c76026c2c77dee8
tobe0504 <ezimorahtobenna@gmail.com>
2022-03-09 22:50:59 +0100

    Added local files from the remix IDE

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.10
+
+Contract HelloWorld{
+    event UpdatedMessages(string oldStr, string newStr)
+
+    string public message;
+
+    constructor (string memory initMessage){
+        message = initMessage
+    }
+
+    function update (string memory newMessage) public {
+        string memory oldMsg = message;
+        message = newMessage;
+        emit UpdatedMessage (oldMsg, newMessage)
+    }
+}
